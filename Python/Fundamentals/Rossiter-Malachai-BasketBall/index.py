9f3ae151527c9ff1ef5290cac41e71059340bebb
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-08-16 14:11:37 -0400

    finished thing

--- Python/Fundamentals/Rossiter-Malachai-BasketBall/index.py
+++ Python/Fundamentals/Rossiter-Malachai-BasketBall/index.py
@@ -1,7 +1,39 @@
 class Player:
-    def __init__(self, name, age, position, team):
-        self.name = name
-        self.age = age
-        self.position = position
-        self.team = team
+    def __init__(self, data):
+        self.name = data['name']
+        self.age = data['age']
+        self.position = data['position']
+        self.team = data['team']
 
+@classmethod
+def add_players(cls, data):
+    player_objects = []
+    for dict in data:
+        player_objects.append(cls(dict))
+    return player_objects
+
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+player_jason = Player(jason)
+player_kevin = Player(kevin)
+player_kyrie = Player(kyrie)
+print(player_jason)
+print(player_kevin)
+print(player_kyrie)

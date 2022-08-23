4483aa20a6bf54a93eba8090f895142de1006bd0
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-08-23 11:39:29 -0400

    made a thing in flask cool

---
+++ Python/Flask/rossiter-malachai-playground/server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/play')
+def level_one():
+    return render_template("index.html", num=3,color="blue")
+
+@app.route('/play/<int:num>')
+def level_one(num):
+    return render_template("index.html", num=num,color="blue")
+
+@app.route('/play/<int:num>/<str:color>')
+def level_one(num,color):
+    return render_template("index.html", num=num,color=color)
+
+if __name__=="__main__":
+    app.run(debug=True)

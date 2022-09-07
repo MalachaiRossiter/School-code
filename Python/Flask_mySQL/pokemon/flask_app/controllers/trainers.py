20227894b690e4ca113b454b40dcf6a0057f574c
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-09-07 13:08:12 -0400

    working on pokemon trainer database

---
+++ Python/Flask_mySQL/pokemon/flask_app/controllers/trainers.py
@@ -0,0 +1,12 @@
+from flask import render_template,redirect,request,session
+
+from flask_app import app
+from flask_app.models.trainer import Trainer
+
+@app.route('/')
+def goIndex():
+    return redirect('/trainers')
+
+@app.route('/trainers')
+def goTrainers():
+    return render_template('index.html')

38915820c3a528a692bb2cc482448092ab62bbc7
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-09-08 22:40:43 -0400

    SOMETHING

--- Python/Flask_mySQL/pokemon/flask_app/controllers/trainers.py
+++ Python/Flask_mySQL/pokemon/flask_app/controllers/trainers.py
@@ -1,7 +1,7 @@
 from flask import render_template,redirect,request,session
 
 from flask_app import app
-from flask_app.models.trainer import Trainer
+#from flask_app.models.trainer import Trainer
 
 @app.route('/')
 def goIndex():

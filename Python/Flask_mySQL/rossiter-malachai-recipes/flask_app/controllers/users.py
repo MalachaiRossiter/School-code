8c6b97de33464654d0abb0a1d0405d25b640d58f
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-09-12 19:32:20 -0400

    fixed page 1

--- Python/Flask_mySQL/rossiter-malachai-recipes/flask_app/controllers/users.py
+++ Python/Flask_mySQL/rossiter-malachai-recipes/flask_app/controllers/users.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, render_template, session, flash
+from flask import Flask, redirect, render_template, session, flash, request
 from flask_app import app
 from flask_app.models.user import User
 
@@ -10,18 +10,22 @@ def login():
 #page actions
 @app.route('/register', methods=['POST'])
 def register():
+    print('request form', request.form)
     valid_user = User.create_user(request.form)
-
     if not valid_user:
-        return redirect("/")
-    
-    session["user_id"] = valid_user.id
+        return redirect('/')
+    session['user_id'] = valid_user.id
     
     return redirect("/recipes/home")
 
-@app.route('/sign-in', methods=['POST'])
+@app.route('/signin', methods=['POST'])
 def sign_in():
+    print('request for', request.form)
+    valid_user = User.authenticate_user(request.form)
+    if not valid_user:
+        return redirect('/')
 
+    session['user_id'] = valid_user.id
     return redirect('/recipies/home')
 
 @app.route('/logout')
--- Python/Flask_mySQL/rossiter-malachai-recipes/flask_app/models/user.py
+++ Python/Flask_mySQL/rossiter-malachai-recipes/flask_app/models/user.py
@@ -16,22 +16,28 @@ class User:
         self.first_name = user['first_name']
         self.last_name = user['last_name']
         self.email = user['email']
+        self.password = user['password']
         self.created_at = user['created_at']
         self.updated_at = user['updated_at']
 
     #Start here to create new User. --- Step 1
     @classmethod
     def create_user(cls, user):
+
         if not cls.validate_user(user):
             return False
 
-        hash = bcrypt.generate_password_hash(user['password'])
-        user['password'] = hash
-
+        parsed_data = {
+            'first_name' : user['first_name'],
+            'last_name' : user['last_name'],
+            'email' : user['email'].lower(), 
+            'password' : bcrypt.generate_password_hash(user['password'])
+        }
+        print("parsed_data", parsed_data)
         query = "INSERT into users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
-        new_user = connectToMySQL(DB).query_db(query,user)
+        new_user = connectToMySQL(DB).query_db(query,parsed_data)
         user_id = cls.get_id(new_user)
-
+        print('user_id', user_id)
         return user_id
 
     @classmethod
@@ -48,7 +54,7 @@ class User:
     #Checks if the email is in the database already. ---Step 3 in Authenticating the New User, Go back to finish step 2
     @classmethod
     def get_email(cls,email):
-        data = {"email": email}
+        data = {"email": email.lower()}
 
         query = "SELECT * FROM users WHERE email = %(email)s;"
         result = connectToMySQL(DB).query_db(query,data)
@@ -59,25 +65,41 @@ class User:
 
     #checking if the username has been filled out propperly ---Step 2 in Authentication
     @classmethod
-    def validate_user(user):
+    def validate_user(cls, user):
         is_valid = True
         if len(user['first_name']) < 2:
             is_valid = False
-            flash('First name needs to be longer, but im not going to say how much longer cause you can use your big brain')
+            flash('First name needs to be longer, but im not going to say how much longer cause you can use your big brain', 'registration')
         if len(user['last_name']) < 3:
             is_valid = False
-            flash('Last Name needs to be longer, but im not going to say how much longer cause you can use your big brain')
+            flash('Last Name needs to be longer, but im not going to say how much longer cause you can use your big brain', 'registration')
         if not EMAIL_REGEX.match(user['email']):
             is_valid = False
-            flash('this aint no email chump')
+            flash('this aint no email chump', 'registration')
         if not user['password'] == user['confirm_password']:
-            flash('yo you cant type very well can you')
+            flash('yo you cant type very well can you', 'registration')
             is_valid = False
-
         #checking for an existing email
         used_email = User.get_email(user['email'])
         if used_email:
-            flash("An account already has that email nerd")
-            valid=False
+            flash("An account already has that email nerd", 'registration')
+            is_valid=False
 
         return is_valid
+
+    #Login method
+    @classmethod
+    def authenticate_user(cls, user):
+
+        existing_user = cls.get_email(user["email"])
+        if not existing_user:
+            flash('this email doesnt exist chump', 'login')
+            return existing_user
+
+        print('password in table: ', existing_user.password)
+        print('password: ', bcrypt.generate_password_hash(user['password']))
+        if not bcrypt.check_password_hash(existing_user.password, user['password']):
+            return False
+        
+        return existing_user
+        

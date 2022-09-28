b96b946266dcf85ea4b6c78f41daba84c205034a
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-09-27 20:13:24 -0400

    Created New Java file

---
+++ Java/Greetings Folder/Greetings.java
@@ -0,0 +1,6 @@
+public class Greetings{
+    public static void main(String[] args){
+        System.out.println("Greetings Earthling, I am from planet Jupitor");
+        System.out.println("California is the place I wish to concor with my 100 years of experience");
+    }
+}

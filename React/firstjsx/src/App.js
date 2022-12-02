2c1cd50583c67a24e4605bf7ed7c0e801579ab11
Malachai Rossiter <malachai.rossiter@gmail.com>
2022-12-02 16:39:27 -0500

    My second react project

---
+++ React/firstjsx/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>These are the things I need to do</h3>
+      <ul>
+        <li>Touch Grass</li>
+        <li>Get beesniches</li>
+        <li>Find the sauce</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;

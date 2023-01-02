5ccb98a18bc6ec81eb933255dfe33eed41309a28
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-01-01 23:52:52 -0500

    First Express App

--- React/Faker_API/server.js
+++ React/Faker_API/server.js
@@ -2,14 +2,57 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-const {faker}
+const {faker} = require('@faker-js/faker');
+
+const createUser = () => {
+    const newFake = {
+        password: faker.internet.password(8),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.number(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        id: faker.datatype.uuid()
+    };
+    return newFake;
+}
+
+const createCompany = () => {
+    const newFake = {
+        id: faker.datatype.uuid(),
+        companyName: faker.company.name(),
+        address: {
+            street: faker.address.streetAddress(),
+            city: faker.address.cityName(),
+            state: faker.address.state(),
+            zipcode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+    };
+    return newFake;
+}
 
 app.get("/api", (req, res) => {
     res.json({message: "BEANS"});
 });
 
 app.get("/api/user", (req, res) => {
-    res.json(users);
-})
+    newUser = createUser();
+    res.json(newUser);
+});
+
+app.get("/api/company", (req, res) => {
+    newCompany = createCompany();
+    res.json(newCompany);
+});
+
+app.get("/api/user/company", (req, res) => {
+    newCompany = createCompany();
+    newUser = createUser();
+    newObject = {
+        user: newUser,
+        company: newCompany
+    };
+    res.json(newObject);
+});
 
 app.listen( port, () => console.log(`Listening on port: ${port}`));
